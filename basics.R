766eeb83ed9184ba0af70eca16267e03261c6814
Harry <harry@HarrydeMacBook-Air.local>
2023-02-16 06:57:50 +0800

    ??

--- basics.R
+++ basics.R
@@ -53,6 +53,25 @@ class(x)
 
 x <- FALSE
 class(x)
+## type conversion
+
+x <- 1L # integer
+y <- 2  # numeric
+
+# convert from integer to numeric:
+a <- as.numeric(x)
+
+# convert from numeric to integer:
+b <- as.integer(y)
+# as.complex(?)
+
+# print values of x and y
+x
+y
+
+# print the class name of a and b
+class(a)
+class(b)
  
 ## Error
 # num <- 5
