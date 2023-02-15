aa94e3130858f5d3f77cc4ce66f3bdd4e646b98b
Harry <harry@HarrydeMacBook-Air.local>
2023-02-16 06:23:05 +0800

    basic R

---
+++ basics.R
@@ -0,0 +1,60 @@
+5 
+10 
+15
+"Hello World~"
+5 + 5
+
+## printing with function
+print("hallow")
+
+##  plotting
+# plot(1:10)
+
+## for loop
+for(i in 1:10){
+	print(i)
+}
+
+## naming variable
+name <- "John"
+age <- 80
+
+## consecutively print (concatenate elements)
+introduction <- paste(name, age, sep="")
+introduction
+
+cat(name, "\n")
+cat(age, "\n")
+
+text <- "aweful"
+paste("R is", text)
+i + age
+
+## multiple samevalue assigning
+
+var1 <- var2 <- var3 <- "Orange"
+var1 
+var2 
+var3
+
+## checking functions' types
+
+x <- 10
+class(x)
+
+x <- 1000L
+class(x)
+
+x <- 9i +3
+class(x)
+
+x <- "R is shitty" 
+class(x)
+
+x <- FALSE
+class(x)
+ 
+## Error
+# num <- 5
+# text <- "Some text"
+# num + text
