6706ec38655db06bcccea77e2e830ac4f39f3444
Harry <harry@HarrydeMacBook-Air.local>
2023-02-16 07:29:14 +0800

    another ?

--- basics.R
+++ basics.R
@@ -72,7 +72,62 @@ y
 # print the class name of a and b
 class(a)
 class(b)
- 
+
+## Mathematical functions
+a <- 10
+b <- -5
+c <- 16
+max(a, b, c)
+min(a, b, c)
+sqrt(c)
+abs(b)
+ceiling(1.4)
+floor(1.4)
+
+## Strings
+stttring <- "Pragmatism"
+stttring
+
+# String Length
+nchar(stttring)
+
+# Checking if ? is in the string
+grepl(i, stttring)
+
+## Global Declaration
+
+x <<- 10
+
+## Miscellaneous Operators
+
+z <- 1:10
+k <- 1:5
+## find out if k belong to z
+k %in% z
+
+## matrix multiplition
+#z %*% k
+
+## if-else Statement
+dice <- 1:6
+
+for(x in dice) {
+  if (x == 6) {
+    print(paste("The dice number is", x, "Yahtzee!"))
+  } else {
+    print(paste("The dice number is", x, "Not Yahtzee"))
+  }
+}
+
+adj <- list("red", "big", "tasty")
+
+fruits <- list("apple", "banana", "cherry")
+  for (x in adj) {
+    for (y in fruits) {
+      print(paste(x, y))
+  }
+}
+
 ## Error
 # num <- 5
 # text <- "Some text"
